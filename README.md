10319f4b5a05430615f624c5195999df40296032
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-05-01 21:09:58 +0500

    Add supabase setup

--- README.md
+++ README.md
@@ -21,10 +21,90 @@ flutter pub get
 - Enable **Email/Password** authentication in Firebase Console
 - Enable **Cloud Firestore** in Firebase Console
 
-### 4. Get SHA Keys (Android)
+#### Get SHA Keys (Android)
 ```bash
 cd android
 ./gradlew signingReport
 ```
 Add the SHA-1 and SHA-256 keys to your Firebase Android app settings.
 
+### 4. Setup Supabase
+1. Create a project in supabase.com
+2. Add package supabase_flutter
+3. URL: Go to settings -> Data API -> API URL which will be url for main
+   AnonKey: Settings -> API Keys -> Secret key
+
+4. Go to https://supabase.com/docs/guides/getting-started/quickstarts/flutter
+5. And for production, you add specific code to android/app/src/main/AndroidManifest.xml for permitting internet usage
+
+#### For storage features
+1. Go to storage.
+2. Add a new bucket and name it
+
+Run using:
+```bash
+flutter pub get
+flutter run --dart-define-from-file=env.json
+```
+
+## 🛠 Tech Stack
+
+Framework: Flutter
+Backend: Firebase (Auth + Firestore)
+State Management: BLoC
+Architecture: BLoC (MVVM-aligned)
+Local Storage: Shared Preferences
+
+
+## Packages used:
+
+1. google_fonts
+2. firebase_core
+3. cloud_firestore
+4. firebase_auth
+5. google_sign_in
+6. equatable
+7. bloc
+8. flutter_bloc
+9. dartz
+10. firebase_messaging
+11. flutter_local_notifications
+12. http
+13. googleapis_auth
+14. app_settings
+15. supabase_flutter
+16. image_picker
+
+### Google sign in setup:
+Add this code to android/build.gradle.kts:
+```bash
+plugins {
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+```
+Add following code to android/app/build.gradle.kts:
+```bash
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+}
+```
+Following to web/index.html under 'head':
+```bash
+  <meta name="google-signin-client_id"
+        content="WEB_CLIENT_ID">
+```
+In main.dart:
+```bash
+if (kIsWeb) {
+    // Web: NO serverClientId — it reads from index.html meta tag instead
+    await GoogleSignIn.instance.initialize();
+  } else if (defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS) {
+    // Mobile: serverClientId required
+    await GoogleSignIn.instance.initialize(
+      serverClientId: 'WEB_CLIENT_ID',
+    );
+  }
+ ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,48 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:lumiere/src/data/datasources/auth_remote_datasource.dart';
+import 'package:lumiere/src/data/datasources/user_remote_datasource.dart';
+import 'package:lumiere/src/data/repositories/auth_repository_impl.dart';
+import 'package:lumiere/src/data/repositories/notification_repository_impl.dart';
+import 'package:lumiere/src/data/repositories/user_repository_impl.dart';
+import 'package:lumiere/src/data/services/notifications/notification_services.dart';
+import 'package:lumiere/src/presentation/blocs/auth/auth_bloc.dart';
+import 'package:lumiere/src/presentation/routers/views.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  //Google sign-in
+  if (kIsWeb) {
+    await GoogleSignIn.instance.initialize();
+  } else if (defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS) {
+    // Mobile: serverClientId required
+    const clientId = String.fromEnvironment('WEB_CLIENT_ID');
+    await GoogleSignIn.instance.initialize(
+      serverClientId: clientId,
+    );
+  }
+
+  const supabase_url = String.fromEnvironment('SUPABASE_URL');
+  const supabase_anon_key = String.fromEnvironment('SUPABASE_ANON_KEY');
+  await Supabase.initialize(
+    url: supabase_url,
+    anonKey: supabase_anon_key,
+  );
+  usePathUrlStrategy();   //For web permissions
+
+
   runApp(const MyApp());
 }
 
@@ -13,9 +50,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LUMIÈRE',
-      //home: SplashScreen(),
+    return BlocProvider(
+      create: (_) => AuthBloc(
+        authRepo: AuthRepositoryImpl(authRemoteDatasource: AuthRemoteDatasource()),
+        userRepo: UserRepositoryImpl(userRemoteDatasource: UserRemoteDatasource()),
+        notificationRepo: NotificationRepositoryImpl(NotificationServices()),
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'LUMIÈRE',
+        home: SplashScreen(),
+      ),
     );
   }
 }
